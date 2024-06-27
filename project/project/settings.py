5350af11349c3bed28101b1bdfe5167a310470a3
Daniel Vieira <danielviei@hotmail.com>
2024-06-27 15:52:51 -0400

    fix: delete GOOGLE_APPLICATION_CREDENTIALS

--- project/project/settings.py
+++ project/project/settings.py
@@ -159,4 +159,3 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-GOOGLE_APPLICATION_CREDENTIALS = env("GOOGLE_APPLICATION_CREDENTIALS")
