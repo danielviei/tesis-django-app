45fbbed9ffd4beb06dec67ae02687638f702f13d
Daniel Vieira <danielviei@hotmail.com>
2024-08-25 14:34:47 -0400

    feat: Update publication form labels and widget

--- project/project/models/publication_forms.py
+++ project/project/models/publication_forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from project.models.publication import Publication
 
 
 class PublicationForm(forms.ModelForm):
     class Meta:
         model = Publication
-        fields = ["title", "image", "content"]
+        fields = ["title", "content", "image"]
+        labels = {
+            "title": _("Título"),
+            "content": _("Contenido"),
+            "image": _("Imagen"),
+        }
+        widgets = {
+            "image": forms.ClearableFileInput(),
+        }
